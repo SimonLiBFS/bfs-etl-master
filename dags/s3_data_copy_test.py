18d4da1d67d4f17eec3dfeb1e36f4bb433c09de9
BrianDai46 <2687126832dcy@gmail.com>
2024-07-11 22:26:55 -0400

    s3 data copy test_2

--- dags/s3_data_copy_test.py
+++ dags/s3_data_copy_test.py
@@ -21,7 +21,7 @@ S3_FILE_PATH = 'product_order_trans_01302024.csv'
 with DAG(
     "s3_data_copy_test",
     start_date=datetime(2022, 7, 13),
-    end_date = datetime(2022, 7, 16),
+    end_date = datetime(2024, 7, 16),
     schedule_interval='0 7 * * *',
     default_args={'snowflake_conn_id': SNOWFLAKE_CONN_ID},
     tags=['beaconfire'],
